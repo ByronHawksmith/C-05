55528f9bf029fefe37438d3d2a431dfd52a83659
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-08-04 05:46:36 +0930

    Complete ex00

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,25 @@
+int	ft_iterative_factorial(int nb)
+{
+	unsigned int	val;
+
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	val = 1;
+	while (nb)
+	{
+		val *= (nb);
+		nb--;
+	}
+	return (val);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int var = 25;
+	printf("factorial(%d) := %d", var, ft_iterative_factorial(var));
+}
+*/
