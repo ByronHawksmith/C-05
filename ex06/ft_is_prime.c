cf5e977665223c60cd7091b8d1eead58383ed7bb
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-08-07 15:09:34 +0930

    Complete ex06

---
+++ ex06/ft_is_prime.c
@@ -0,0 +1,57 @@
+int	ft_sqrt(int nb)
+{
+	int	i;
+	int	sqrt;
+
+	if (nb <= 0)
+		return (0);
+	if (nb == 1)
+		return (1);
+	i = 0;
+	sqrt = 0;
+	while (sqrt <= nb)
+	{
+		i++;
+		sqrt = i * i;
+	}
+	i -= 1;
+	sqrt = i * i;
+	if (sqrt == nb)
+		return (i);
+	else
+		return (0);
+}
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+	int	sqrt;
+
+	if (nb <= 1)
+		return (0);
+	if (nb <= 3)
+		return (1);
+	if (nb % 2 == 0 || nb % 3 == 0)
+		return (0);
+	i = 2;
+	sqrt = ft_sqrt(nb);
+	while ((i <= sqrt) && (nb % i != 0))
+		i++;
+	return (i > sqrt);
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	printf("0: %d\n", ft_is_prime(0));
+	printf("1: %d\n", ft_is_prime(1));
+	printf("2: %d\n", ft_is_prime(2));
+	printf("3: %d\n", ft_is_prime(3));
+	printf("4: %d\n", ft_is_prime(4));
+	printf("5: %d\n", ft_is_prime(5));
+	printf("239: %d\n", ft_is_prime(239));
+	printf("271: %d\n", ft_is_prime(271));
+	printf("300: %d\n", ft_is_prime(300));
+}
+*/
