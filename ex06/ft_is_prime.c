1094f03ad5c4f3ab9a3867301d64be8d7f9a6a2f
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-08-10 13:31:13 +0930

    Simplify ex06

--- ex06/ft_is_prime.c
+++ ex06/ft_is_prime.c
@@ -1,44 +1,22 @@
-int	ft_sqrt(int nb)
-{
-	int	i;
-	int	int_max_sqrt;
-
-	if (nb <= 0)
-		return (0);
-	if (nb == 1)
-		return (1);
-	i = 0;
-	int_max_sqrt = 46341;
-	while (i <= nb / 2 && i <= int_max_sqrt)
-	{
-		if (i * i == nb)
-			return (i);
-		i++;
-	}
-	return (0);
-}
-
 int	ft_is_prime(int nb)
 {
 	int	i;
-	int	sqrt;
 
-	if (nb <= 1)
-		return (0);
-	if (nb <= 3)
-		return (1);
-	if (nb % 2 == 0 || nb % 3 == 0)
+	if (nb < 2)
 		return (0);
 	i = 2;
-	sqrt = ft_sqrt(nb);
-	while ((i <= sqrt) && (nb % i != 0))
+	while (i <= nb / 2)
+	{
+		if (nb % i == 0)
+			return (0);
 		i++;
-	return (i > sqrt);
+	}
+	return (1);
 }
 
 /*
 #include <stdio.h>
-int main(void)
+int	main(void)
 {
 	printf("0: %d\n", ft_is_prime(0));
 	printf("1: %d\n", ft_is_prime(1));
